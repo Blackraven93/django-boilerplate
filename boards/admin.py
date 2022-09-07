e5f306fd42e5eea001fc3658f705a7be7b3ca610
Blackraven93 <reblackraven@gmail.com>
2022-09-08 00:32:52 +0900

    Feat: init Boards model and admin

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Board
 
-# Register your models here.
+
+@admin.register(Board)
+class BoardAdmin(admin.ModelAdmin):
+
+    list_display = (
+        "title",
+        "author",
+        "description",
+    )
+    # list_filter = []
+    search_fields = ("author",)
--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Board(models.Model):
+    """Board Model"""
+
+    title = models.CharField(max_length=140)
+    author = models.CharField(max_length=20)
+    description = models.TextField(max_length=140)
+
+    def __str__(self):
+        return self.title
